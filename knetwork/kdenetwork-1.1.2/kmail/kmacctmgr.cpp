23b116c0cde2b5c7ec879ba1511f044ebab9737f
Carlos Uliao <culiao@opencores.org>
2014-03-12 16:07:31 +0000

    Minor header fixes in kmail

--- knetwork/kdenetwork-1.1.2/kmail/kmacctmgr.cpp
+++ knetwork/kdenetwork-1.1.2/kmail/kmacctmgr.cpp
@@ -15,7 +15,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <iostream.h>
+#include <iostream>
 
 extern KBusyPtr *kbp;
 //-----------------------------------------------------------------------------
