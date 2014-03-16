91c3407b5cd2cc3d0e420d568589049bed38358a
Carlos Uliao <culiao@opencores.org>
2014-03-16 00:22:47 +0000

    kdenetwork should build now

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 ( cd kdegames ; sh kdegames.build )
 ( cd kgraphic ; sh kdegraphics.build )
 ( cd kmedia ; sh kdemultimedia.build )
-#( cd knetwork ; ./kdenetwork.build )
+( cd knetwork ; sh kdenetwork.build )
 #( cd kdetoys ; ./kdetoys.build )
 #( cd kdeutils ; ./kdeutils.build )
 #( cd korganiz ; ./korganizer.build )
